9a59e77edb7b305273f91a09ed51c8a06d5035f0
Malay Kumar <malaykumar2003@gmail.com>
2024-05-02 19:34:56 +0530

    Fix: csvReader and add sample.csv

--- src/csvReader.js
+++ src/csvReader.js
@@ -1,45 +1,20 @@
-const fs = require("fs");
-const csv = require("csv-parser");
-const { parse } = require("json2csv");
-const hll = require("hll");
+const fs = require('fs');
+const csv = require('csv-parser');
 
 function readCSV(filePath) {
-  const results = [];
-  var h = hll();
+    const results = [];
 
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(filePath)
-      .pipe(csv())
-      .on("data", (data) => results.push(data))
-      .on("end", () => {
-        resolve(results);
-      })
-      .on("error", (error) => {
-        reject(error);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(filePath)
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                resolve(results);
+            })
+            .on('error', (error) => {
+                reject(error);
+            });
+    });
 }
 
-function readCSVForHLL(filePath, bitSampleSize = 12, digestSize = 128) {
-  const results = [];
-  var h = hll({ bitSampleSize: bitSampleSize, digestSize: digestSize });
-
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(filePath)
-      .pipe(csv())
-      .on("data", (data) => h.insert(JSON.stringify(data)))
-      .on("end", () => {
-        resolve(h);
-      })
-      .on("error", (error) => {
-        reject(error);
-      });
-  });
-}
-
-async function writeCSV(filename, data) {
-  const csv = parse(data);
-  fs.writeFileSync(filename, csv);
-}
-
-module.exports = { readCSV, readCSVForHLL, writeCSV };
+module.exports = readCSV;
